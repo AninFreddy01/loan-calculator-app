ded86c28f585be25a6e51084034e8cbb0b4eac26
Godfred Osei <aninfreddy01@gmail.com>
2021-06-24 16:12:32 +0000

    input validate update

--- script.js
+++ script.js
@@ -13,10 +13,33 @@ let calculateBtn = document.getElementById('calculate');
 //calculation function
 calculateBtn.addEventListener('click', function(){
     
-    //formulars
+
+    //validate if input value is null
+    if (loanAmount.value === "") {
+        document.getElementById('error').innerHTML = "Enter a value!";
+    } else {
+        document.getElementById('error').innerHTML = "";
+    }
+    if (interestPercent.value === "") {
+        document.getElementById('error2').innerHTML = "Enter a value!";
+    } else {
+        document.getElementById('error2').innerHTML = "";
+    }
+    if (monthsToPay.value === "") {
+        document.getElementById('error3').innerHTML = "Enter a value!";
+    } else {
+        document.getElementById('error3').innerHTML = "";
+    }
+    
+    if (loanAmount.value != "" && interestPercent.value != "" && monthsToPay.value != "") {
+
+        //formulars
     let interestRate = document.getElementById('interest_rate').innerHTML = Math.round((interestPercent.value / 100) * loanAmount.value);
     let totalLoanAmount =  document.getElementById('total_loan_amount').innerHTML = Math.round(parseInt( loanAmount.value) + interestRate) ;
-    document.getElementById('amount_per_month').innerHTML = Math.round(parseInt(totalLoanAmount) / monthsToPay.value);
+    let amountPerMonth = document.getElementById('amount_per_month').innerHTML = Math.round(parseInt(totalLoanAmount) / monthsToPay.value);
+    }
+    
+    
 });
 
 //intialization of clear button
@@ -30,5 +53,3 @@ clearBtn.addEventListener('click', function(){
     interestPercent.value = "";
     monthsToPay.value = "";
 });
-
-
